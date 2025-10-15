2c6d1317c115e40ea66c1d889cfa76587e526adb
iblac142 <113121929+iblac142@users.noreply.github.com>
2025-10-15 15:28:01 -0600

    job struct

--- jobs.h
+++ jobs.h
@@ -2,11 +2,22 @@
 #define JOBS_H
 
 #define MAX_ARGS 64  
+#define MAX_PIPELINE_LEN 64
 
 struct Command
 {
   char *argv[MAX_ARGS+1];
   unsigned int argc;
 };
+
+struct Job
+{
+  struct Command pipeline[MAX_PIPELINE_LEN];
+  unsigned int num_stages;
+  char *outfile_path;		/* NULL for no output redirection */
+  char *infile_path;		/* NULL for no input redirection */
+  int background;			/* 0 for foreground, 1 for background */
+}
+
 
 #endif
