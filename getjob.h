d21b056315ecd52d8ae8ff5c1cffe68f71b0e626
iblac142 <113121929+iblac142@users.noreply.github.com>
2025-10-19 22:27:45 -0600

    update function description

--- getjob.h
+++ getjob.h
@@ -10,12 +10,13 @@ job - the job structure to be populated
 	
 Returns:
   1 if an exit command is detected
-  0 if run successful
+  0 if run successful and no exit command is detected
   1 if error due to too many characters
   -2 if error due to too many arguments
   -3 if error due to too many pipeline stages
   -4 if error due to malformed command 
 */
 int get_job(struct Job* job);
+
 
 #endif
